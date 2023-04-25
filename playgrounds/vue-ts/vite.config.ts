11ce3649b039a7c0025a69d559a0381bfa4f22a4
xiaoxian521 <1923740402@qq.com>
2023-04-26 01:21:27 +0800

    chore: update playgrounds

--- playgrounds/vue-ts/vite.config.ts
+++ playgrounds/vue-ts/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import removeConsole from "../../dist/index.mjs";
+import removeConsole from "vite-plugin-remove-console";
+// import removeConsole from "../../dist/index.mjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
